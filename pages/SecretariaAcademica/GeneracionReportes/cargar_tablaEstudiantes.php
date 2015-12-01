03b994f78302c9981c15ba6d3f13e476e01c9f39
hllanosp <hllanos75@gmail.com>
2015-12-01 14:42:44 -0600

    agregado cambios actividades 5 y 6

--- pages/SecretariaAcademica/GeneracionReportes/cargar_tablaEstudiantes.php
+++ pages/SecretariaAcademica/GeneracionReportes/cargar_tablaEstudiantes.php
@@ -14,12 +14,20 @@
                    <th style='width: 600px'>Nombre Estudiante</td>
                    <th>correo</td>
                    <th> Indice Academico</td>
+
+                   <th>UV acumulados</th>
                    <th>Mencion Honorifica</td>
                    <th>Solicitudes</td>
                    <th>Plan de Estudio</td>
                    <th>Orientacion</td>
                    <th>Ciudad Origen</td>
                    <th>Residencia Actual</td>
+                   <th>Grupo Etnico</th>
+                   <th>Anos Certificado</th>
+                   <th>Anos en Derecho</th>
+                   <th>Carrera Anterior</th>
+
+
                 </tr>
             </thead>
             <tbody id = 'tabla_filtrada'>";
@@ -33,6 +41,13 @@
     $orientacion = obtenerOrientacion($row['cod_orientacion']);
     $ciudad_origen = obtenerCiudadOrigen($row['cod_ciudad_origen']);
     $residencia_actual = obtenerRecidencia($row['cod_residencia_actual']);
+     $grupo_etnico = $row['grupo_etnico'];
+     $anios_inicio_estudio = $row['anios_inicio_estudio'];
+     $anios_final_estudio = $row['anios_final_estudio'];
+     $carrera_anterior = $row['carrera_anterior'];
+     $uv_acumulados = $row['uv_acumulados'];
+    
+
    
     echo  "<tr>".
                 "<td id='".$row['no_cuenta']."'>".$row['no_cuenta']."</td>".
@@ -40,13 +55,18 @@
                 "<td id='".$row['no_cuenta']."'>".utf8_encode($estudiante)."</td>".
                 "<td id='".$row['dni']."'>".$correo."</td>".
                 "<td id='".$row['indice_academico']."'>".$row['indice_academico']."</td>".
+                "<td id=''>".$row['uv_acumulados']."</td>".
+
                 "<td id='".$mencion."'>".utf8_encode($mencion)."</td>".
                 "<td id=''>".$solicitudes."</td>".
                 "<td id=''>".utf8_encode($planEstudio)."</td>".
                 "<td id=''>".utf8_encode($orientacion)."</td>".
                 "<td id=''>".utf8_encode($ciudad_origen)."</td>".
                 "<td id=''>".utf8_encode($residencia_actual)."</td>".
-              
+                "<td id=''>".utf8_encode($grupo_etnico)."</td>".
+                "<td id=''>".utf8_encode($anios_inicio_estudio)."</td>".
+                "<td id=''>".utf8_encode($anios_final_estudio)."</td>".
+                "<td id=''>".utf8_encode($carrera_anterior)."</td>".
                   "</tr>";
   }
   echo "</tbody>
