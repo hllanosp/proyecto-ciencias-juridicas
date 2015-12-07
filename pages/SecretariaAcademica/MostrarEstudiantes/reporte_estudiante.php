b5c88b4c89d163b2c14e0e87e046f5f16fd6d599
Edwin504 <edwin_m_paz@hotmail.com>
2015-12-06 18:43:53 -0600

    Cambios al formato de reportes de estudiantes
    Se dio formato al reporte de estudiante.

--- pages/SecretariaAcademica/MostrarEstudiantes/reporte_estudiante.php
+++ pages/SecretariaAcademica/MostrarEstudiantes/reporte_estudiante.php
@@ -27,19 +27,49 @@ if(isset($_SESSION["TIPO_SOLICITUD"]))
 
 
             
-$pdf = new FPDF();
+$pdf = new FPDF('P', 'mm', array(215.9, 330.2));
+$pdf->AddFont('Calibri', 'B', 'calibrib.php');
+$pdf->AddFont('Cambria', 'BI', 'cambriaz.php');
+$pdf->AddFont('Cambria', 'I', 'cambriai.php');
+$pdf->AddFont('Cambria', 'B', 'cambriab.php');
+$pdf->AddFont('Cambria', '', 'Cambria.php');
+
 
 $pdf->AddPage();
 $pdf->SetFont('Arial', '', 18);
-$pdf->Image($maindir.'assets/img/cabecera.png', -2,5,212,30, 'PNG');
-$pdf->Image($maindir.'assets/img/lucen-aspicio.png', 78,85,200,200, 'PNG');
+$pdf->Image($maindir.'assets/img/Encabezado de documentos.jpg', 4.0, 0.5, 209.6, 32.2, 'JPG');
+$pdf->Image($maindir."assets/img/Pie de documentos.jpg", 4.0, 191, 208.8, 137, 'JPG');
+
+$pdf->SetRightMargin(15);
+$pdf->SetLeftMargin(15);
+$pdf->SetFont('Calibri', 'B', 11);
+$pdf->Cell(0, -4.5, utf8_decode("FACULTAD DE CIENCIAS JURÍDICAS"), 0, 0, 'R');
+$pdf->Ln(2);
+$pdf->SetFont('Arial', '', 10);
+$pdf->Cell(0, 0, utf8_decode("Teléfono: 2232-2290"), 0, 1, 'R');
+$pdf->Ln(4.1);
+$pdf->Cell(0, 0, utf8_decode("Edificio A-2"), 0, 1, 'R');
+$pdf->Ln(4.1);
+$pdf->Cell(0, 0, utf8_decode("Ciudad Universitaria"), 0, 1, 'R');
+$pdf->Ln(4.1);
+$pdf->Cell(0, 0, utf8_decode("Tegucigalpa, Honduras"), 0, 1, 'R');
+
+$pdf->SetRightMargin(10);
+$pdf->SetLeftMargin(10);
+
 $pdf->Cell(18, 10, '', 0);
-$pdf->Ln(30);
-$pdf->SetFont('Arial', 'U', 14);
-$pdf->Cell(30, 8, ' ', 0,0,"C");
-$pdf->Cell(130, 8, ' Reporte de Estudiantes', 0,0,"C");
-$pdf->Rect(2, 55, 206, 200 ,'D');
+//$pdf->Ln(20);
+//$pdf->SetFont('Arial', 'U', 14);
+//$pdf->Cell(30, 8, ' ', 0,0,"C");
+//$pdf->Cell(130, 8, ' Reporte de Estudiantes', 0,0,"C");
+
+$pdf->ln(20);
+$pdf->SetFont('Cambria', 'BI', 16);
+$pdf->Cell(0, 0, utf8_decode("Reporte de Estudiantes"), 0, 1, 'C');
+
+$pdf->Rect(5, 55, 206, 200 ,'D');
 $pdf->SetFont('Arial', '', 12);
+
 $pdf->Ln(15);
 $pdf->Cell(115, 15, 'Fecha: '.date('Y-m-d'), 0);
 $pdf->Ln(7);
@@ -51,7 +81,7 @@ $pdf->Cell(30, 8, 'Identidad',1, 0,"C");
 $pdf->Cell(45, 8, 'Estudiante',1, 0,"C");
 $pdf->Cell(40, 8, 'Correo',1, 0,"C");
 $pdf->Cell(10, 8, 'Indice',1, 0,"C");
-$pdf->Cell(33, 8, 'Mencion Horifica',1, 0,"C");
+$pdf->Cell(33, 8, utf8_decode('Mención Horifica'),1, 0,"C");
 $pdf->Cell(10, 8, 'Cant',1, 0,"C");
 $pdf->Ln(8);
 $pdf->SetFont('Arial', '', 8);
@@ -93,13 +123,13 @@ while ($fila = mysql_fetch_array($result))
     $correo = obtenerCorreo($fila['dni']);
     $mencion = obtenerMencion($fila['dni']);
     $solicitudes = obtenercantidadSolicitudes($fila['dni']);
-    $pdf->Cell(25, 8, $fila['no_cuenta'],1,0,"C");
-    $pdf->Cell(30, 8, $fila['dni'],1,0,"C");
-    $pdf->Cell(45, 8, $estudiante,1,0,"C");
-    $pdf->Cell(40, 8, $correo,1,0,"C");
-    $pdf->Cell(10, 8, $fila['indice_academico']."%",1,0,"C");
-    $pdf->Cell(33, 8, $mencion,1,0,"C");
-    $pdf->Cell(10, 8, $solicitudes,1,0,"C");
+    $pdf->Cell(25, 8, utf8_decode($fila['no_cuenta']),1,0,"C");
+    $pdf->Cell(30, 8, utf8_decode($fila['dni']),1,0,"C");
+    $pdf->Cell(45, 8, utf8_decode($estudiante),1,0,"C");
+    $pdf->Cell(40, 8, utf8_decode($correo) ,1,0,"C");
+    $pdf->Cell(10, 8, utf8_decode($fila['indice_academico'])."%",1,0,"C");
+    $pdf->Cell(33, 8, utf8_decode($mencion),1,0,"C");
+    $pdf->Cell(10, 8, utf8_decode($solicitudes),1,0,"C");
 
     $pdf->Ln(8);   
     
@@ -107,7 +137,7 @@ while ($fila = mysql_fetch_array($result))
 }
 
 $pdf->SetFont('Arial', '', 10);
-$pdf->Image($maindir.'assets/img/pieDepagina.png', -2,260,212,40, 'PNG');
+//$pdf->Image($maindir.'assets/img/pieDepagina.png', -2,260,212,40, 'PNG');
 $pdf->Output();   
 
     function obtenerEstudiante($dni){
